6ca1ee916c362551e824d67f1c26ca70eea74946
rihanbinmonzur <rihanidblass@gmail.com>
2025-08-11 07:32:54 +0600

    users

--- src/Admin/Users.js
+++ src/Admin/Users.js
@@ -1,9 +1,18 @@
 import Adminlayout from '../layout/Adminlayout';
+import axios from 'axios';
+import React from 'react';
 import {Link} from 'react-router-dom';
 function Users(){
+    const [users,setUsers] =React.useState([])
+    axios.get(`http://localhost/christmas-api/users.php`).then(function(response){
+        setUsers(response.data);
+    });
     return(
         <Adminlayout>
+           <div className='container'>
+           
         <h1>Users</h1>
+        <Link to={'/admin/add-users'} className="btn btn-primary">Add new</Link>
         <table className="mt-5 table table-bordered">
             <tr>
               <th>#SL</th>
@@ -12,7 +21,18 @@ function Users(){
               <th>Email</th>
               <th>Action</th>
             </tr>
-        </table>
+            {users && users.map((d, key) =>
+            <tr key={key}>
+                <td className="text-bold-500">{key +1}</td>
+                <td>{d.name}</td>
+                <td>{d.contact_no}</td>
+                <td>{d.email}</td>
+               <td>
+                </td>
+                </tr>
+                )}
+        </table> 
+        </div>
         </Adminlayout>
     );
 }
